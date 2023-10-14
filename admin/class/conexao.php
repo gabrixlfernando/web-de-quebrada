0cd4643f92ec041bdd61cd72403b309e5ecf167d
gabrixlfernando <biel_nando2012@hotmail.com>
2023-10-14 00:15:10 -0300

    .

--- admin/class/conexao.php
+++ admin/class/conexao.php
@@ -4,8 +4,8 @@ class Conexao
 {
     public static function LigarConexao()
     {
-         $conn = new PDO('mysql:dbname=dbwebdequebrada;host=localhost', 'root', '');
-       // $conn = new PDO('mysql:dbname=u283879542_webdequebrada;host=smpsistema.com.br', 'u283879542_webdequebrada', 'SenacAgencia01');
+        $conn = new PDO('mysql:dbname=dbwebdequebrada;host=localhost', 'root', '');
+        //$conn = new PDO('mysql:dbname=u283879542_webdequebrada;host=smpsistema.com.br', 'u283879542_webdequebrada', 'SenacAgencia01');
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     }
